39bd30c747a3805d5ee92fb504c12ba1783bce9f
paosanchezmonroy <paola_sanchez_monroy@hotmail.com>
2023-05-22 17:30:40 -0600

    toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ListComponentComponent } from './movies/list-component/list-component.component';
 import { ListadoGenericoComponent } from './utilidades/listado-generico/listado-generico.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialModule} from './material/material.module'
+import {MaterialModule} from './material/material.module';
+import { MenuComponent } from './menu/menu.component'
 @NgModule({
   declarations: [
     AppComponent,
     ListComponentComponent,
-    ListadoGenericoComponent
+    ListadoGenericoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatToolbarModule} from '@angular/material/toolbar'
 
 
 @NgModule({
   declarations: [],
+  exports:[
+    MatToolbarModule
+  ],
   imports: [
     CommonModule
   ]
